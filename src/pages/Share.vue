d72af28fccde674144e7212c81b8f773a262b5ba
jasonxs45 <jasonxs45@126.com>
2018-06-28 19:48:55 +0800

    修改页面分享逻辑

--- src/pages/Share.vue
+++ src/pages/Share.vue
@@ -5,12 +5,28 @@
 </template>
 <script>
 import api from 'common/api'
+import wx from 'weixin-js-sdk'
 export default {
   name: 'Share',
   data () {
     return {
       id: '',
       src: 'static/images/sample.png'
+    }
+  },
+  computed: {
+    wxReady () {
+      return this.$store.state.global.wxReady
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.id = to.params.id
+    },
+    'wxReady' (newVal, oldVal) {
+      if (newVal) {
+        this.changeShare()
+      }
     }
   },
   methods: {
@@ -37,11 +53,23 @@ export default {
           })
         }
       })
+    },
+    changeShare () {
+      let shareData = {
+        title: '推荐绑定得积分',
+        desc: '绑定金地客服公众号，畅享服务与活动！',
+        imgUrl: 'http://jindi.1juke.cn/jindiMicro/static/images/share1.png'
+      }
+      wx.onMenuShareAppMessage(shareData)
+      wx.onMenuShareTimeline(shareData)
     }
   },
   created () {
     this.id = this.$route.params.id
     this.getData()
+    if (this.$store.state.global.wxReady) {
+      this.changeShare()
+    }
   }
 }
 </script>
